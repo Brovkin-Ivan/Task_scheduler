64e5d1a356e18071b04e979950b557f4091e64c0
Brovkin Ivan <isbrovkinn@gmail.com>
2023-05-16 16:46:37 +0300

    Add test and jococo

--- src/test/java/ru/netology/services/TaskTest.java
+++ src/test/java/ru/netology/services/TaskTest.java
@@ -30,6 +30,15 @@ class TaskTest {
     }
 
     @Test
+    public void testMatchesEpicWhenTrue2() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Молоко");
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
     public void testMatchesEpicWhenFalse() {
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
         Epic epic = new Epic(55, subtasks);
@@ -53,5 +62,22 @@ class TaskTest {
         boolean actual = meeting.matches("2й версии приложения");
         Assertions.assertFalse(actual);
 
+    }
+
+    @Test
+    public void testMatchesMeetingProjectWhenTrue() {
+        Meeting meeting = new Meeting(555, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
+
+        boolean actual = meeting.matches("НетоБанка");
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void testMatchesMeetingProjectWhenFalse() {
+        Meeting meeting = new Meeting(555, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
+
+        boolean actual = meeting.matches("НедоБанка");
+        Assertions.assertFalse(actual);
     }
 }
